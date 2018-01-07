05afda9bc6092e1d18a05edd9eb50e8477c11fde
Harry Ng <harryworld@gmail.com>
2018-01-07 18:22:57 +0800

    Add Episode 1 Storyboards

---
+++ 1-Storyboards/Storyboards/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Storyboards
+//
+//  Created by Harry Ng on 7/1/2018.
+//  Copyright © 2018 Harry Ng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
