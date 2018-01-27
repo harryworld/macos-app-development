e1f72d09d37c991520442f1641519adf325a68eb
Harry Ng <harryworld@gmail.com>
2018-01-28 07:20:22 +0800

    Add NSCollectionView initial project

---
+++ 3-NSCollectionView/NSCollectionView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSCollectionView
+//
+//  Created by Harry Ng on 28/1/2018.
+//  Copyright © 2018 Harry Ng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
